194f087036aaedfc7e1a92f579beff601f22d913
Ovie Ekas <ovikat4life@gmail.com>
2020-04-25 14:00:27 +0100

    Add project files.

---
+++ NotelyProject/Notelyenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\muneer\Documents\djangoprojects\Notely\NotelyProject\Notelyenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
